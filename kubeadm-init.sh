fe01c56140dfeb603c52747840fa28ea0c5e6f0b
Hubbe <HubbeKing@users.noreply.github.com>
2019-11-28 22:58:13 +0200

    Fetch calico config from projectcalico.org

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -8,8 +8,10 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# set up calico pod networking
-kubectl apply -f calico_v3.10_custom_cidr.yaml
+# set up calico pod networking for the above pod-network-cidr and apply
+wget https://docs.projectcalico.org/v3.10/manifests/calico.yaml
+sed -i 's|192.168.0.0/16|10.0.0/16|g' calico.yaml
+kubectl apply -f calico.yaml
 
 # allow scheduling pods on control-plane nodes
 kubectl taint nodes --all node-role.kubernetes.io/master-
