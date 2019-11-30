0a8bfa7b659c3a15df581ab3645c6331b199d2fd
HubbeKing <hubbe128@gmail.com>
2019-11-30 17:26:11 +0200

    Fix nginx secret generation script

--- create-nginx-secret.sh
+++ create-nginx-secret.sh
@@ -3,4 +3,4 @@
 CERT_PATH=$1
 KEY_PATH=$2
 
-kubectl create secret --namespace=nginx-ingress tls --cert="${CERT_PATH}" --key="${KEY_PATH}" --dry-run --output=yaml --save-config > nginx/certificate.yaml
+kubectl create secret tls --namespace=nginx-ingress tls --cert="${CERT_PATH}" --key="${KEY_PATH}" --dry-run --output=yaml --save-config > nginx/certificate.yaml
