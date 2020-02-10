273c52343eb08962de0564cc024fff5e757477c5
Hubbe <HubbeKing@users.noreply.github.com>
2020-02-10 12:01:41 +0200

    Update setup-cert-manager.sh

--- setup-cert-manager.sh
+++ setup-cert-manager.sh
@@ -3,6 +3,7 @@
 kubectl create namespace cert-manager
 
 # https://cert-manager.io/docs/installation/upgrading/#upgrading-using-static-manifests
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.13.0/cert-manager.yaml
-
+cd core
+wget https://github.com/jetstack/cert-manager/releases/download/v0.13.0/cert-manager.yaml
+kubectl apply -f cert-manager.yaml
 kubectl apply -f cert-issuer/
