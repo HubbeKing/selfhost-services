9f845007e1ccf0a46e5698302b5375d755d6a69b
Hubbe <HubbeKing@users.noreply.github.com>
2020-02-12 12:13:02 +0200

    Delete setup-cert-manager.sh

--- setup-cert-manager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace cert-manager
-
-# https://cert-manager.io/docs/installation/upgrading/#upgrading-using-static-manifests
-cd core
-wget https://github.com/jetstack/cert-manager/releases/download/v0.13.0/cert-manager.yaml
-kubectl apply -f cert-manager.yaml
-kubectl apply -f cert-issuer/
