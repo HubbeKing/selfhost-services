bc8dc2438f5638d3a0ceed5b1f918ac32c31dbe8
Hubbe <HubbeKing@users.noreply.github.com>
2020-02-12 10:54:16 +0200

    Use recommended settings for k8s docker

--- setup-docker.sh
+++ setup-docker.sh
@@ -3,8 +3,15 @@
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  }
   "storage-driver": "overlay2"
 }
 EOF
 
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
 systemctl restart docker
