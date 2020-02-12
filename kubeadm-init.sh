0f72974362d4007beb35327e95d8010ee732dbb6
Hubbe <HubbeKing@users.noreply.github.com>
2020-02-12 11:49:07 +0200

    Use repo flannel manifest in kubeadm init script

--- kubeadm-init.sh
+++ kubeadm-init.sh
@@ -13,8 +13,7 @@ sudo sysctl net.ipv4.conf.all.rp_filter=1
 
 # by default, Flannel uses 10.244.0.0/16
 # so long as both Flannel and kubeadm agree on the CIDR it shouldn't matter much
-# change if this conflicts with other things
-DESIRED_CIDR=10.0.0.0/16
+DESIRED_CIDR=10.244.0.0/16
 
 # Initialize kubernetes control-plane on this machine
 sudo kubeadm init --pod-network-cidr=${DESIRED_CIDR}
@@ -24,8 +23,5 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# apply Flannel pod networking manifest, ensure it's adjusted for $DESIRED_CIDR
-cd core
-wget https://raw.githubusercontent.com/coreos/flannel/v0.11.0/Documentation/kube-flannel.yml
-sed -i 's|10.244.0.0/16|'${DESIRED_CIDR}'|g' kube-flannel.yml
-kubectl apply -f kube-flannel.yml
+# apply Flannel pod networking manifest
+kubectl apply -f core/kube-flannel.yml
