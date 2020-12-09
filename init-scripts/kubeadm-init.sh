a6ea50020e26416ff5a05a51d1e1ba9ba2cd2d14
Hubbe <HubbeKing@users.noreply.github.com>
2020-12-09 19:03:20 +0200

    Reverse CONTROL_PLANE_ENDPOINT behavior
    This makes the script a bit more readable

--- init-scripts/kubeadm-init.sh
+++ init-scripts/kubeadm-init.sh
@@ -10,14 +10,15 @@ sudo systemctl enable kubelet.service
 # get required kubeadm config vars from INSTALL_SETTINGS
 source INSTALL_SETTINGS
 
-if [ -n "${CONTROL_PLANE_ENDPOINT}" ]; then
-    # Initialize single control-plane cluster on this machine
-    KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} envsubst < kubeadm-configs/single.yaml > temp.yaml
-    sudo kubeadm init --config temp.yaml
-else
-    # Initialize stacked HA control-plane cluster on this machine
+# check if a CONTROL_PLANE_ENDPOINT has been specified
+if [ -z "${CONTROL_PLANE_ENDPOINT}" ]; then
+    # Initialize stacked HA control-plane cluster on this machine if so
     KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} CONTROL_PLANE_ENDPOINT=${CONTROL_PLANE_ENDPOINT} envsubst < kubeadm-configs/stacked-ha.yaml > temp.yaml
     sudo kubeadm init --upload-certs --config temp.yaml
+else
+    # Initialize single control-plane cluster on this machine otherwise
+    KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} envsubst < kubeadm-configs/single.yaml > temp.yaml
+    sudo kubeadm init --config temp.yaml
 fi
 
 # set up kubectl for non-sudo use by copying config into home
