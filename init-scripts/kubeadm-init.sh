f8d2d297e530b7849198f4278a3aacd2e3ead190
HubbeKing <hubbe128@gmail.com>
2020-12-09 10:23:08 +0200

    Fix kubeadm config handling

--- init-scripts/kubeadm-init.sh
+++ init-scripts/kubeadm-init.sh
@@ -12,10 +12,12 @@ source INSTALL_SETTINGS
 
 if [ -n "${CONTROL_PLANE_ENDPOINT}" ]; then
     # Initialize single control-plane cluster on this machine
-    KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} envsubst < kubeadm-configs/single.yaml | sudo kubeadm init --config -
+    KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} envsubst < kubeadm-configs/single.yaml > temp.yaml
+    sudo kubeadm init --config temp.yaml
 else
     # Initialize stacked HA control-plane cluster on this machine
-    KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} CONTROL_PLANE_ENDPOINT=${CONTROL_PLANE_ENDPOINT} envsubst < kubeadm-configs/stacked-ha.yaml | sudo kubeadm init --upload-certs --config -
+    KUBE_VERSION=${KUBE_VERSION} POD_NETWORK_CIDR=${POD_NETWORK_CIDR} K8S_SERVICE_CIDR=${K8S_SERVICE_CIDR} CONTROL_PLANE_ENDPOINT=${CONTROL_PLANE_ENDPOINT} envsubst < kubeadm-configs/stacked-ha.yaml > temp.yaml
+    sudo kubeadm init --upload-certs --config temp.yaml
 fi
 
 # set up kubectl for non-sudo use by copying config into home
@@ -25,3 +27,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # apply Project Calico pod networking manifest
 kubectl apply -f ../core/calico.yaml
+
+# remove temporary kubeadm config file
+rm temp.yaml
